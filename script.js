8f813700ddcf719a5d27039c5b94c6c86a15cd0f
yuriiprots <uraprots7849@gmail.com>
2024-01-05 20:30:15 +0200

    create container for new div elements

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function createDivs(numberOfDivs) {
+  const container = document.getElementById("myContainer");
+
+  if (!container) {
+    console.log("Container not found");
+    return;
+  }
+
   for (let i = 0; i < numberOfDivs; i++) {
     const newDiv = document.createElement("div");
+    newDiv.className = "div";
     newDiv.innerHTML = "This is a new div!";
-    document.body.appendChild(newDiv);
+    container.appendChild(newDiv);
   }
 }
 
