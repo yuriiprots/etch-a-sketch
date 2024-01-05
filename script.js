c5bad3fa79afb623be08c73b8ccd9b54838584b7
yuriiprots <uraprots7849@gmail.com>
2024-01-05 20:16:42 +0200

    create the 'createDivs' function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function createDivs(numberOfDivs) {
+  for (let i = 0; i < numberOfDivs; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "This is a new div!";
+    document.body.appendChild(newDiv);
+  }
+}
 
+createDivs(5);
