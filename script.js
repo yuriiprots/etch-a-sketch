6bfa0a6ec0141e40454f769cf9788f5dc82ef088
yuriiprots <uraprots7849@gmail.com>
2024-01-06 13:25:54 +0200

    add a hover effect so that the grid divs change colour when the mouse passes over them, leaving a (pixelated) trail through the grid like a pen

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const container = document.getElementById("myContainer");
+let isMouseDown = false;
+
+function addColour() {
+  this.classList.add("color-effect");
+}
 
 function createDivs(numberOfDivs) {
   if (!container) {
@@ -9,9 +14,24 @@ function createDivs(numberOfDivs) {
   for (let i = 0; i < numberOfDivs; i++) {
     const newDiv = document.createElement("div");
     newDiv.className = "div";
-    // newDiv.innerHTML = "This is a new div!";
+    newDiv.addEventListener("click", addColour);
+    newDiv.addEventListener("mousemove", function () {
+      if (isMouseDown) this.classList.add("color-effect");
+    });
+    newDiv.addEventListener("dragstart", function (event) {
+      event.preventDefault();
+    });
+
     container.appendChild(newDiv);
   }
 }
+
+document.addEventListener("mousedown", function () {
+  isMouseDown = true;
+});
+
+document.addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
 
 createDivs(256);
