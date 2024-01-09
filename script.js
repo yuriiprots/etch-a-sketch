1828300af9371f366d21306862ee4a464321f5e6
yuriiprots <uraprots7849@gmail.com>
2024-01-09 19:11:12 +0200

    create the 'cleanGrid' function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container = document.getElementById("myContainer");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const cleanBtn = document.getElementById("cleanBtn");
+const columns = container.querySelectorAll(".column");
+
 let isMouseDown = false;
+
+createGrid(16, 16);
+
+sizeSlider.addEventListener("input", function () {
+  sizeValue.textContent = `${this.value} x ${this.value}`;
+  createGrid(this.value, this.value);
+});
 
 function addColour() {
   this.classList.add("colour-effect");
@@ -10,7 +22,7 @@ function createGrid(numberOfColumn, numberOfDivsPerColumn) {
     console.log("Container not found");
     return;
   }
-
+  cleanGrid();
   for (let i = 0; i < numberOfColumn; i++) {
     const newColumn = document.createElement("div");
     newColumn.className = "column";
@@ -33,6 +45,56 @@ function createGrid(numberOfColumn, numberOfDivsPerColumn) {
   }
 }
 
+// function cleanColor(numberOfColumn, numberOfDivsPerColumn) {
+//   if (!container) {
+//     console.log("Container not found");
+//     return;
+//   }
+//   const columns = container.children;
+//   for (let i = 0; i < numberOfColumn; i++) {
+//     const column = columns[i];
+//     for (let j = 0; j < numberOfDivsPerColumn; j++) {
+//       if (column.children[j].classList.contains("colour-effect")) {
+//         const cell = column.children[j];
+//         cell.classList.remove("colour-effect");
+//       }
+//     }
+//   }
+// }
+
+// function cleanGrid(numberOfColumn) {
+//   if (!container) {
+//     console.log("Container not found");
+//     return;
+//   }
+//   // container.removeChild(columns[0]);
+
+//   while (numberOfColumn >= 0) {
+//     container.removeChild(columns[0]);
+//     numberOfColumn--;
+//   }
+// }
+
+function cleanGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+// const columns = container.children;
+// for (let i = 0; i < numberOfColumn; i++) {
+//   const column = columns[i];
+
+//   for (let j = 0; j < numberOfDivsPerColumn; j++) {
+//     if (column.children[j].classList.contains("colour-effect")) {
+//       const cell = column.children[j];
+//       cell.classList.remove("colour-effect");
+//     }
+//   }
+
+cleanBtn.addEventListener("click", cleanColor(numberOfColumn, numberOfDivsPerColumn));
+
+
 document.addEventListener("mousedown", function () {
   isMouseDown = true;
 });
@@ -40,6 +102,3 @@ document.addEventListener("mousedown", function () {
 document.addEventListener("mouseup", function () {
   isMouseDown = false;
 });
-
-createGrid(15, 15);
-
